1a555686ed303b9cc6acb35b307fa9af844fed4d
Gary Boone <gary.boone@gmail.com>
2011-06-19 16:13:46 -0700

    reorganized demos

--- demos/regression/regression_demo.go
+++ demos/regression/regression_demo.go
@@ -15,66 +15,6 @@ import (
 
 const NUM_SAMPLES = 5
 
-func incrementalDemo() {
-	var d stats.Desc
-	fmt.Printf("\n**** Descriptive Statistics, Incremental Updates **\n")
-	fmt.Printf("** create a new descriptive stats struct:\n")
-	fmt.Printf("var d stats.Desc\n")
-	fmt.Printf("** update it with new values:\n")
-	for i := 0; i < NUM_SAMPLES; i++ {
-		x := rand.Float64()*100.0 - 25.0 // uniform samples in {-25, 75}
-		fmt.Printf("d.Update(%v)\n", x)
-		d.Update(x)
-	}
-	fmt.Printf("then request stats, for example:    d.Count()\n")
-	fmt.Printf("\n** Descriptive Statistics:\n")
-	fmt.Printf("count = %v\n", d.Count())
-	fmt.Printf("min = %v\n", d.Min())
-	fmt.Printf("max = %v\n", d.Max())
-	fmt.Printf("sum = %v\n", d.Sum())
-	fmt.Printf("mean = %v\n", d.Mean())
-	fmt.Printf("standard deviation = %v\n", d.SampleStandardDeviation())
-	fmt.Printf("variance = %v\n", d.SampleVariance())
-	skew := d.SampleSkew()
-	fmt.Printf("skew = %v\n", skew)
-	// 
-	// The rand functions return uniformly distributed values. Therefore, with
-	// enough samples, the skew should be 0. Try increasing NUM_SAMPLES
-	if skew < 0.0 {
-		fmt.Printf("  The sample is skews left (longer tail to left).\n")
-	} else if skew > 0.0 {
-		fmt.Printf("  The sample is skews right (longer tail to right).\n")
-	}
-	kurtosis := d.SampleKurtosis()
-	fmt.Printf("kurtosis = %v\n", kurtosis)
-	// 
-	// The rand functions return uniformly distributed values. Therefore, with
-	// enough of them, our sample should be flat, or platykurtic.
-	if kurtosis < 0.0 {
-		fmt.Printf("  The sample is platykurtic.\n")
-	} else if kurtosis > 0.0 {
-		fmt.Printf("  The sample is leptokurtic.\n")
-	}
-}
-
-func batchDemo() {
-	fmt.Printf("\n**** Descriptive Statistics, Batch Updates **\n")
-	fmt.Printf("** array of values:\n")
-	a := makeArray(NUM_SAMPLES, 100, -25)
-	printArray("x", a)
-	fmt.Printf("}\n")
-	fmt.Printf("then request stats, for example:    stats.StatsCount(a)\n")
-	fmt.Printf("** Descriptive Statistics:\n")
-	fmt.Printf("count = %v\n", stats.StatsCount(a))
-	fmt.Printf("min = %v\n", stats.StatsMin(a))
-	fmt.Printf("max = %v\n", stats.StatsMax(a))
-	fmt.Printf("sum = %v\n", stats.StatsSum(a))
-	fmt.Printf("mean = %v\n", stats.StatsMean(a))
-	fmt.Printf("standard deviation = %v\n", stats.StatsSampleStandardDeviation(a))
-	fmt.Printf("variance = %v\n", stats.StatsSampleVariance(a))
-	fmt.Printf("skew = %v\n", stats.StatsSampleSkew(a))
-	fmt.Printf("kurtosis = %v\n", stats.StatsSampleKurtosis(a))
-}
 
 func incrementalRegressionDemo() {
 	var r stats.Regression
@@ -146,8 +86,6 @@ func main() {
 
 	rand.Seed(time.Nanoseconds())
 
-	incrementalDemo()
-	batchDemo()
 	incrementalRegressionDemo()
 	batchRegressionDemo()
 }
